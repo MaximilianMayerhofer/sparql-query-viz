34bbf5ccd15c165543106a5a3a77ed1bb9f0c94a
Maximilian Mayerhofer <ga27yuj@mytum.de>
2022-01-10 18:01:36 +0100

    fix: refactoring of folder 'example_ontology' (formerly known as 'got') fix: load_ontology code has now descriptions and comments fix: small code adjustments in load_ontology to increase readability

--- jaal/datasets/load_ontology.py
+++ jaal/datasets/load_ontology.py
@@ -32,6 +32,8 @@ def load_got(filter_conections_threshold=10):
     return edge_df, node_df
 
 def load_ontology():
+    """Loads the Pizza-example ontology wih classes, with classes, instances, object- and data-properties"""
+
     this_dir, _ = os.path.split(__file__)
     iri = "http://example.org/onto-got.owl"
     fname = "./onto-got.owl"
@@ -47,128 +49,250 @@ def load_ontology():
            ["Her_pizza", "quattro_stagioni", "diameter_in_cm", "32", "integer"], \
            ["Her_pizza", "quattro_stagioni", "description", "jane's pizza", "string"]]
     ontor1.add_axioms(classes)
-    ontor1.add_ops(ontor.load_json(os.path.join(this_dir, "got", "props.json"))["op"])
-    ontor1.add_dps(ontor.load_json(os.path.join(this_dir, "got", "props.json"))["dp"])
-    ontor1.add_axioms(ontor.load_csv(os.path.join(this_dir, "got", "class_axioms.csv")))
+    ontor1.add_ops(ontor.load_json(os.path.join(this_dir, "example_ontology", "props.json"))["op"])
+    ontor1.add_dps(ontor.load_json(os.path.join(this_dir, "example_ontology", "props.json"))["dp"])
+    ontor1.add_axioms(ontor.load_csv(os.path.join(this_dir, "example_ontology", "class_axioms.csv")))
     ontor1.add_instances(ins)
     return ontor1
 
-def get_tboxes(onto: OntoEditor):
-    nodelist = []
+def get_tboxes(onto: OntoEditor, nodelist = []):
+    """Extracts T-Boxes from ontology and returns them in a list.
+
+    Parameters
+    -----------
+    onto: OntoEditor
+        ontology of type OntoEditor from which classes are extracted
+    nodelist: list
+        list of all classes/ instances that were already extracted from the ontology"""
+
+    # Generator of all classes in the ontology is created
     node_gen = onto.onto.classes()
+    # All classes from the generator are written into a list with their name, importance, shape and T-Box label
     for cl in node_gen:
-        nodelist.append([cl.name, 1, 'dot', 'T'])  # 'T' indicates that this is a tBox
+        nodelist.append([cl.name, 1, 'dot', 'T'])
+    #return list of all extracted classes
     return nodelist
 
-def get_isa_realtions(onto: OntoEditor):
-    edgelist = []
-    node_gen = onto.onto.classes()  # mit list keyword kann mehrfach verwendet werden
+def get_isa_realtions(onto: OntoEditor, edgelist = []):
+    """Extracts all 'is_a'-relations from ontology and returns them in a list.
+
+    Parameters
+    -----------
+    onto: OntoEditor
+        ontology of type OntoEditor from which relations are extracted
+    edgelist: list
+        list of all relations that were already extracted from the ontology"""
+
+    # Generator of all classes in the ontology is created
+    node_gen = onto.onto.classes()
+    # For all classes from the generator the associated subclasses are written into a list
     for cl in node_gen:
         rellist = list(cl.subclasses())
+        # All subclasses are written into a list with their name, associated superclasses' name,
+        # id, weight, label and dashed-boolean
         for i, value in enumerate(rellist):
-            edgelist.append([rellist[i].name, cl.name, 'is_a', 1, 'is_a', False])
+            identifier = rellist[i].name + ' is_a ' + cl.name
+            edgelist.append([rellist[i].name, cl.name, identifier, 1, 'is_a', False])
+    # return list of all extracted edges/ relations
     return edgelist
 
-def get_OPs(onto: OntoEditor, edgelist):
+def get_OPs(onto: OntoEditor, edgelist = []):
+    """Extracts all object-properties from ontology and returns them in a list that is passed to the function.
+
+    Parameters
+    -----------
+    onto: OntoEditor
+        ontology of type OntoEditor from which relations are extracted
+    edgelist: list
+        list of all relations that were already extracted from the ontology"""
+
+    # Generator of all object-properties in the ontology is created
     op_gen = onto.onto.object_properties()
+    # Iteration over all object-properties from the generator
     for op in op_gen:
-        op_name = op.name
-        op_dom = op.domain
-        op_ran = op.range
-        for i, value in enumerate(op_ran):
+        # All range-elements (targets) are written into a list except the first element, with their
+        # domain's name, name, identifier, weight, associated object-property's name and dashes-boolean
+        for i, value in enumerate(op.range):
             if i != 0:
-                edgelist.append([op_dom[0].name, op_ran[i].name, op_name, 1, op_name, True])
+                identifier = op.domain[0].name + ' ' + op.name + ' ' + op.range[i].name
+                edgelist.append([op.domain[0].name, op.range[i].name, identifier, 1, op.name, True])
+    # return list of all extracted edges/ relations
     return edgelist
 
-def get_DPs(onto: OntoEditor, nodelist, edgelist):
+def get_DPs(onto: OntoEditor, nodelist = [], edgelist = []):
+    """Extracts all data-properties from ontology and returns them in a list that is passed to the function.
+
+    Parameters
+    -----------
+    onto: OntoEditor
+        ontology of type OntoEditor from which relations are extracted
+    nodelist: list
+        list of all classes/ instances that were already extracted from the ontology
+    edgelist: list
+        list of all relations that were already extracted from the ontology"""
+
+    # Generator of all data-properties in the ontology is created
     dp_gen = onto.onto.data_properties()
+    # Iteration over all data-properties from the generator
     for dp in dp_gen:
-        dp_name = dp.name
-        dp_dom = dp.domain
+        # A list of unique domains of the associated data-property is created
         dp_dom_unique = []
-        for dom in dp_dom:
+        for dom in dp.domain:
             if not dom in dp_dom_unique:
                 dp_dom_unique.append(dom)
-        dp_ran = dp.range
         try:
-            flag_nodes = False
-            flag_edges = False
-            dp_type = str(dp_ran).split("'")[1]
-            for edge_column in edgelist:
+            # Booleans to indicate, whether node/ edge is already in nodelist/ edgelist are instantiated
+            node_in_nodelist = False
+            edge_in_edgelist = False
+            # The datatype of the associated data-property is written into a string-variable
+            dp_type = str(dp.range).split("'")[1]
+            # Iteration over all relations in edgelist
+            for edge in edgelist:
+                # Iteration over all unique domains of the associated data-property
                 for i, value in enumerate(dp_dom_unique):
-                    if edge_column[0] == dp_dom_unique[i].name and edge_column[1] == dp_type and (
+                    # If there is already a relation between two corresponding domains and data-types,
+                    # and it is not the first domain of the associated data-property (unless there is only one),
+                    # the new data-property is added to the existing edge with their id and the data-property's name.
+                    # Also the weight of the edge is increased by one and the boolean to indicate the edge already
+                    # exists is set to True
+                    if edge[0] == dp_dom_unique[i].name and edge[1] == dp_type and (
                             len(dp_dom_unique) == 1 or not i == 0):
-                        flag_edges = True
-                        edge_column[2] = edge_column[2] + ', ' + dp_name
-                        edge_column[4] = edge_column[4] + ', ' + dp_name
-                        edge_column[3] = edge_column[3] + 1
-            if not flag_edges:
+                        edge_in_edgelist = True
+                        identifier = dp_dom_unique[i].name + ' ' + dp.name + ' ' + dp_type
+                        edge[2] = edge[2] + ', ' + identifier
+                        edge[4] = edge[4] + ', ' + dp.name
+                        edge[3] = edge[3] + 1
+            # If there is not already a relation between two corresponding domains and data-types,
+            # and it is not the first domain of the associated data-property (unless there is only one), the new
+            # data-property is written to a list with their domain's name, data-type, id, weight, data-property's name
+            # and dashes-boolean
+            if not edge_in_edgelist:
                 for i, value in enumerate(dp_dom_unique):
                     if len(dp_dom_unique) == 1 or not i == 0:
-                        edgelist.append([dp_dom_unique[i].name, dp_type, dp_name, 1, dp_name, True])
-
+                        identifier = dp_dom_unique[i].name + ' ' + dp.name + ' ' + dp_type
+                        edgelist.append([dp_dom_unique[i].name, dp_type, identifier, 1, dp.name, True])
+            # Iteration over all nodes in nodelist
             for node in nodelist:
+                # If the data-type of the associated data-property is already in the nodelist, the boolean to indicate
+                # the node already exists is set to True
                 if node[0] == dp_type:
-                    flag_nodes = True
-            if not flag_nodes:
+                    node_in_nodelist = True
+            # If node_in_list is False, the data-type of the associated data-property will be added to the nodelist
+            # with their identifier, weight, shape and T-Box label
+            if not node_in_nodelist:
                 nodelist.append([dp_type, 1, 'triangle', 'T'])
-
+        # If an IndexError is thrown, an warning will be shown, that the one data-property was skipped
+        # (not added to the edgelist)
         except IndexError:
-            print(dp_name, "was skipped because the range of", dp_name, "is empty or could not be interpreted.")
+            print(dp.name, "was skipped because the range of", dp.name, "is empty or could not be interpreted.")
+    # return list of all extracted nodes/ data-types and list of all extracted edges/ relations
     return nodelist, edgelist
 
 def get_node_weights(node_df, edge_df):
-    is_there_a_weight_col = False
-    for node_col in node_df.columns:
-        if node_col == 'importance':
-            is_there_a_weight_col = True
+    """Weights the nodes in nodelist acoording to the number of incoming edges
 
-    if is_there_a_weight_col:
-        i = 0
-        for node in node_df['id']:
-            count = 0
-            for index, edge in edge_df.iterrows():
-                if node == edge['to'] and edge['id'] == 'is_a':
-                    count = count + 1
-            if count != 0:
-                node_df.loc[i,'importance'] = count
-            i = i + 1
+    Parameters
+    -----------
+    node_df: pandas.DataFrame
+        DataFrame parsed from nodelist
+    edge_df: pandas.DataFrame
+        DataFrame parsed from edgelist"""
 
+    # Boolean to indicate there is an weight/ importance column in node_df is created
+    importance_col = False
+    # If there is a column in node_df called importance, importance_col will be set to True
+    for col in node_df.columns:
+        if col == 'importance':
+            importance_col = True
+    # If importance_col is True ...
+    if importance_col:
+        # Iteration over nodes in node_df
+        for node_index, node in node_df.iterrows():
+            # For every new node-ID the counter is set to zero
+            counter = 0
+            # Iteration over all edges in edge_df
+            for edge_index, edge in edge_df.iterrows():
+                # If the ID/ name of a node is equal to the edge to-column and it is an is_a relation,
+                # the counter is increased by one
+                if node['id'] == edge['to'] and edge['label'] == 'is_a':
+                    counter = counter + 1
+            # If the counter of a node is higher than zero, the new importance value is assigned
+            if counter != 0:
+                node_df.loc[node_index,'importance'] = counter
+    # return node_df with new importance values
     return node_df
 
 def get_aboxes(onto: OntoEditor, nodelist, edgelist):
-    node_gen = onto.onto.classes()
-    for node in node_gen:
-        ins_list = onto.onto.search(type = node)
-        for ins in ins_list:
-            flag_nodes = False
-            flag_edge = False
+    """Extracts all instances/ A-Boxes from ontology and returns them in a list that is passed to the function.
 
+    Parameters
+    -----------
+    onto: OntoEditor
+        ontology of type OntoEditor from which relations are extracted
+    nodelist: list
+        list of all classes/ instances that were already extracted from the ontology
+    edgelist: list
+        list of all relations that were already extracted from the ontology"""
+
+    # Generator of all classes in the ontology is created
+    node_gen = onto.onto.classes()
+    # Iteration over all classes/ nodes in the generator
+    for node in node_gen:
+        # Iteration over all instances of of the associated class
+        for ins in onto.onto.search(type = node):
+            # Booleans to indicate, whether node/ edge is already in nodelist/ edgelist are instantiated
+            node_in_nodelist = False
+            edge_in_edgelist = False
+
+            # TODO: write data-/ object-properties in edge-list
             for prop in ins.get_properties():
                 for value in prop[ins]:
                     prop_value = value
-
+            # Get superclass of instance
             superclass = ins.is_a
+            # Iterate over all classes/ nodes in nodelist
             for cl in nodelist:
+                # If there is a class in nodelist that has the same name as the instance node_in_list is set to True
                 if cl[0] == ins.name:
-                    flag_nodes = True
-            if not flag_nodes:
+                    node_in_nodelist = True
+            # If node_in_nodelist is False the instance is written in a list with its ID, weight, shape and A-Box label
+            if not node_in_nodelist:
                 nodelist.append([ins.name, 1, 'box', 'A'])
+            # Iteration over all relations/ edges in edgelist
             for rel in edgelist:
+                # If there is already an edge between the instance and its superclass, edge_in_edgelist is set to True.
+                # The new relation is added to the existing edge with its ID and label and the weight
+                # is increased by one.
                 if ins.name == rel[0] and superclass[0].name == rel[1]:
-                    flag_edge = True
-            if not flag_edge:
-                edgelist.append([ins.name, superclass[0].name, 'is_a', 1, 'is_a', False])
+                    edge_in_edgelist = True
+                    identifier = ins.name + ' is_a ' + superclass[0].name
+                    rel[2] = rel[2] + ', ' + identifier
+                    rel[4] = rel[4] + ', ' + 'is_a'
+                    rel[3] = rel[3] + 1
+            # If edge_in_edgelist is False, the new instance is written into a list
+            # with its name, its superclasses' name, identifier, weight, label and dashes-boolean
+            if not edge_in_edgelist:
+                identifier = ins.name + ' is_a ' + superclass[0].name
+                edgelist.append([ins.name, superclass[0].name, identifier, 1, 'is_a', False])
+    # return list of all extracted instances and list of all extracted edges/ relations
     return nodelist, edgelist
 
 def get_df_from_ontology(onto: OntoEditor, abox: bool = False):
+    """Parses the information given by the ontology into a panda.DataFrame
+
+    Parameters
+    -----------
+    onto: OntoEditor
+       ontology of type OntoEditor from which the information is extracted
+    abox: bool
+        Boolean to indicate wheter instances should be included or not"""
+
     nodelist = get_tboxes(onto)
     edgelist = get_isa_realtions(onto)
     edgelist = get_OPs(onto, edgelist)
     nodelist, edgelist = get_DPs(onto, nodelist, edgelist)
     if abox:
         nodelist, edgelist = get_aboxes(onto, nodelist, edgelist)
-
     node_df = pd.DataFrame(nodelist)
     node_df.columns = ['id', 'importance', 'shape', 'T/A']
     edge_df = pd.DataFrame(edgelist)
